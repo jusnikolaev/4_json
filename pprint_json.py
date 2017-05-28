71c84a00dd285944f44ac504770641d714f7a07f
Iurii Nikolaev <jusnikolaev@gmail.com>
2017-05-28 11:39:58 +0300

    Bug fix

--- pprint_json.py
+++ pprint_json.py
@@ -1,25 +1,25 @@
 import json
+import os
 import argparse
 
 
 def load_data(filepath):
-    try:
-        with open(filepath, 'r') as file:
-            data = json.load(file)
-            return data
-    except FileNotFoundError:
+    if not os.path.exists(filepath):
         print('No file in directory')
         exit()
+    with open(filepath, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
 
 
-def pretty_print_json(data):
-    parsed_json = json.dumps(data, ensure_ascii=False, indent=4,
+def pretty_print_json(file_json):
+    parsed_file = json.dumps(file_json, ensure_ascii=False, indent=4,
                              sort_keys=True)
-    return parsed_json
+    return parsed_file
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Filepath')
+    parser = argparse.ArgumentParser(description='File path')
     parser.add_argument('--path', type=str)
     args = parser.parse_args()
     parsed_json = load_data(args.path)
